d5300d6853364c38de475f3d8512e7695f6370ba
Yash Gupta <yashg160@gmail.com>
2021-11-07 00:31:34 +0530

    ADD: Create New Link Scenario

--- routes/api/links/index.js
+++ routes/api/links/index.js
@@ -2,6 +2,7 @@ const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const randomstring = require('randomstring');
 
 const sequelize = require('../../../sequelize');
 
@@ -27,6 +28,56 @@ router.get('/', async (req, res, next) => {
     });
   } catch (err) {
     console.log('Link Data Fetching Error', err);
+    return res.status(500).json({
+      message: 'A server error occurred. Try again later',
+    });
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    let errors = [];
+
+    if (!_.has(req.body, 'sourceURL'))
+      errors.push('Source URL is a required field');
+    if (
+      !/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g.test(
+        req.body.sourceURL
+      )
+    )
+      errors.push('Source URL should be of a valid format');
+
+    if (errors.length !== 0) {
+      return res.status(400).json({
+        message: 'Errors were found with the data',
+        errors: errors,
+      });
+    }
+
+    // Generate random string of length 10 to use as slug
+    const generatedSlug = randomstring.generate(10);
+
+    try {
+      const sql1 = `INSERT INTO links (user_id, source, slug) VALUES (${res.locals.dataPayload.id}, '${req.body.sourceURL}', '${generatedSlug}');`;
+
+      const [[r1, m1]] = await Promise.all([sequelize.query(sql1)]);
+
+      return res.status(200).json({
+        data: {
+          id: r1,
+          slug: generatedSlug,
+          source: req.body.sourceURL,
+        },
+        message: 'Created new link',
+      });
+    } catch (err) {
+      console.log('Link Creating Error', err);
+      return res.status(500).json({
+        message: 'A server error occurred. Try again later',
+      });
+    }
+  } catch (err) {
+    console.log('Link Creating Error', err);
     return res.status(500).json({
       message: 'A server error occurred. Try again later',
     });
